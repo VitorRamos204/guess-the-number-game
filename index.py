48cf694f704b8e74bfc0ec4e044a95af531f9cfa
Vitor Ramos de Senna <vitor.ramos2759@gmail.com>
2022-04-23 10:10:16 -0300

    new function (secretnumber)

--- index.py
+++ index.py
@@ -1,20 +1,23 @@
-from random import randint
+def secretnumber(a=0):
+        from random import randint
+        print('-=-'*30)
+        print('                           Guess The Number Game                       ')
+        print('-=-'*30) 
+        a = randint(1, 10)
+        return a
+        
 
-secretnumber = randint(1, 10)
+secret = secretnumber()
 attempts = 5
-print('-=-'*30)
-print('                           Guess The Number Game                       ')
-print('-=-'*30)
 while True:
     choice = int(input('Guess the number (1 at 10): '))
-    if choice == secretnumber:
+    if choice == secret:
         print('Great! you choice the secret number')
         break
-    elif choice != secretnumber:
+    elif choice != secret:
         attempts -= 1
         print(f'Wrong number, try again, you have {attempts} attempts')
         if attempts == 0:
             break
-    else:
-        print(f'The right number is {secretnumber}')
+print(f'The right number is {secret}')
 print('Thank you for playing Guess The Number, if you wanna play again just start the software again :D"')
