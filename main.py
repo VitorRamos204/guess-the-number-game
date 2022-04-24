bbd3bccee63be4e789653a519d3af10b59c307fc
Vitor Ramos de Senna <vitor.ramos2759@gmail.com>
2022-04-24 11:18:34 -0300

    newgame function

--- main.py
+++ main.py
@@ -5,19 +5,30 @@ def secretnumber(a=0):
         print('-=-'*30) 
         a = randint(1, 10)
         return a
-        
 
-secret = secretnumber()
-attempts = 5
-while True:
-    choice = int(input('Guess the number (1 at 10): '))
-    if choice == secret:
-        print('Great! you choice the secret number')
-        break
-    elif choice != secret:
-        attempts -= 1
-        print(f'Wrong number, try again, you have {attempts} attempts')
-        if attempts == 0:
+
+def game():
+    secret = secretnumber()
+    attempts = 5
+    while True:
+        choice = int(input('Guess the number (1 at 10): '))
+        if choice == secret:
+            print('Great! you choice the secret number')
             break
-print(f'The right number is {secret}')
-print('Thank you for playing Guess The Number, if you wanna play again just start the software again :D"')
+        elif choice != secret:
+            attempts -= 1
+            print(f'Wrong number, try again, you have {attempts} attempts')
+            if attempts == 0:
+                break
+
+
+# Main
+game()
+while True:
+    newgame = str(input(('Play again? \n1. Yes \n2. No\n Y/N: ')))
+    if newgame in 'Yy':
+        game()
+    else:
+        print('Thank you for playing Guess The Number =)')
+        break
+       
